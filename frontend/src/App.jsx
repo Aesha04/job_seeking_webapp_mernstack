c1db3f27977e0a3163e51b6bddd492c8855f5084
Aesha04 <110104348+Aesha04@users.noreply.github.com>
2024-04-07 20:48:38 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/getuser",
+          "http://job-seeking-webapp-mernstack-backend.vercel.app/api/v1/user/getuser",
           {
             withCredentials: true,
           }
